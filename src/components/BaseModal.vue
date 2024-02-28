277e4929a5ec990bb0c025c90b583df72b5e0474
LBJ Ubuntu <1325349854@qq.com>
2024-02-28 18:30:14 +0800

    BaseModal基本实现(子父页面传参)

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <Transition name="modal-outer">
+    <div
+      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      v-show="modalActive"
+    >
+      <Transition name="modal-inner">
+        <div
+          v-if="modalActive"
+          class="p-4 bg-white self-start mt-32 max-w-screen-md"
+        >
+          <slot />
+          <button
+            class="text-white mt-8 bg-weather-primary py-2 px-6"
+            @click="$emit('close-modal')"
+          >
+            Close
+          </button>
+        </div>
+      </Transition>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+defineEmits(["close-modal"]); // 定义向包含该页面提交的模型
+defineProps({
+  // 定义包含该页面的类
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped>
+.modal-outer-enter-active {
+}
+
+.modal-outer-leave-active {
+}
+</style>
--- src/components/BaseModel.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="absolute "></div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
