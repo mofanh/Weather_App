da4e023086daacf93ac60c3efe8aa6053231c9ba
LBJ Ubuntu <1325349854@qq.com>
2024-02-28 18:45:40 +0800

    Teleport:保存不占用页面空间; Tranition:动态变换

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,25 +1,27 @@
 <template>
-  <Transition name="modal-outer">
-    <div
-      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-      v-show="modalActive"
-    >
-      <Transition name="modal-inner">
-        <div
-          v-if="modalActive"
-          class="p-4 bg-white self-start mt-32 max-w-screen-md"
-        >
-          <slot />
-          <button
-            class="text-white mt-8 bg-weather-primary py-2 px-6"
-            @click="$emit('close-modal')"
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+        v-show="modalActive"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
           >
-            Close
-          </button>
-        </div>
-      </Transition>
-    </div>
-  </Transition>
+            <slot />
+            <button
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+              @click="$emit('close-modal')"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script setup>
@@ -38,5 +40,27 @@ defineProps({
 }
 
 .modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-inner-enter-from {
+  opacity: 0;
+  transition: scale(0.8);
+}
+
+.modal-inner-leave-to {
+  transition: scale(0.8);
 }
 </style>
