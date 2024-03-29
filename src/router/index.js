cf21b8a0ca018d887179c14cdfeebf04741b5069
lier <86977618+mofanh@users.noreply.github.com>
2024-03-29 10:45:57 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/h",
       name: "home",
       component: HomeView,
       meta: {
